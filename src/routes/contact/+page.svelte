205bec89f6d966d16ac1c02dba8701e3ffae68b3
Max Veytsman <maxim@ontoillogical.com>
2023-06-11 15:29:19 -0400

    Contact page

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,15 @@
+
+<svelte:head>
+  <title>1RG Labs: Contact Us</title>
+</svelte:head>
+
+<div id="hero" class="flex flex-col w-full bg-black ">
+  <div class="w-full flex flex-col justify-center items-center bg-black  h-screen relative">
+    <div class="text-white mt-16 md:mt-0 text-2xl md:text-4xl lg:text-6xl font-bold mb-12">Want to work with us?</div>
+    <section class="flex justify-center bg-black pb-24">
+      <p class="max-w-4xl text-md md:text-xl xl:text-2xl font-light leading-normal text-center text-gray-300 px-12">
+        Get in touch at <a class="underline hover:text-mediumorchid" href="mailto:team@1rg.space">team@1rg.space</a>
+      </p>
+    </section>
+  </div>
+</div>
