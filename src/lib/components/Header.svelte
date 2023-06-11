83c99787627f0f71c52f730dde25f979314b4167
Max Veytsman <maxim@ontoillogical.com>
2023-06-11 15:29:24 -0400

    Update link in header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -11,8 +11,8 @@
 		<Logo class="justify-center h-20 lg:ml-24" />
 	</a>
 	<div class="flex items-center justify-end w-full">
-		<div class="text-white mr-8 flex items-center text-xl font-bold uppercase">
-				<a class="hover:text-darkseagreen" href="https://1rg.space/">1RG.Space</a>
+		<div class="text-white mr-2 md:mr-8 flex items-center text-lg md:text-xl font-bold uppercase">
+				<a class="hover:text-darkseagreen" href="https://1rg.space/">1RG Space</a>
 	  </div>
 	  <HamburgerButton on:click={() => (menuOpen = true)} />
 	</div>
