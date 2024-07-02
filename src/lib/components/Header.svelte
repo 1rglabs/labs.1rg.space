6c32d170edd0a9b4f5af12eba23e4d4a0f48d9e5
Max Veytsman <maxim@ontoillogical.com>
2024-07-02 17:04:38 -0400

    Add social icons

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -2,6 +2,8 @@
 	import Logo from '$lib/components/Logo.svelte';
 	import HamburgerButton from '$lib/components/HamburgerButton.svelte';
 	import Menu from './Menu.svelte';
+	import IconMastadon from '~icons/fa6-brands/mastodon';
+	import IconInstagram from '~icons/fa6-brands/instagram';
 
 	let menuOpen = false;
 </script>
@@ -11,10 +13,30 @@
 		<Logo class="justify-center h-20 lg:ml-24" />
 	</a>
 	<div class="flex items-center justify-end w-full">
-		<div class="text-white mr-2 md:mr-8 flex items-center text-lg md:text-xl font-bold uppercase">
-				<a class="hover:text-darkseagreen" href="https://1rg.space/">1RG Space</a>
-	  </div>
-	  <HamburgerButton on:click={() => (menuOpen = true)} />
+		<div
+			class="text-white mr-2 md:mr-8 flex items-center text-lg md:text-xl font-bold uppercase space-x-2"
+		>
+			<a
+				class="hover:text-darkseagreen"
+				href="https://instagram.com/1rg.space"
+				rel="me"
+				target="_blank"
+				title="Follow 1RG on Instagram"
+			>
+				<IconInstagram />
+			</a>
+			<a
+				class="hover:text-darkseagreen"
+				href="https://cosocial.ca/@1rg"
+				rel="me"
+				target="_blank"
+				title="Follow 1RG on Mastadon"
+			>
+				<IconMastadon />
+			</a>
+			<a class="hover:text-darkseagreen" href="https://1rg.space/">1RG Space</a>
+		</div>
+		<HamburgerButton on:click={() => (menuOpen = true)} />
 	</div>
 </header>
 <Menu bind:open={menuOpen} />
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 import { imagetools } from 'vite-imagetools'
+import Icons from 'unplugin-icons/vite';
 
 
 const config: UserConfig = {
-	plugins: [imagetools(), sveltekit()]
+	plugins: [imagetools(), sveltekit(),  Icons({ compiler: 'svelte', autoInstall: true })]
 };
 
 export default config;
